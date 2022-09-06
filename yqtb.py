674d1c616a847b2beb19a90f42951c96f94ef4c8
a251231 <55580477+a251231@users.noreply.github.com>
2022-09-06 09:29:58 +0800

    Update yqtb.py

--- yqtb.py
+++ yqtb.py
@@ -62,7 +62,7 @@ def punchForm(form, session):
     message9='夫祸患常积于忽微，而智勇多困于所溺！【每日健康提醒】打卡'+str(res_json)+' 加油奥利给！！@face=201@@face=201@ ' #想发送啥消息自己改
     text={ "content":message9}
     data9={"msgtype": "text",'text':text}
-    re=requests.post(url=url9,headers=headers9,data=data9)
+    re=requests.post(url=url9,headers=headers9,data=json.dumps(data9))
     print(re.json())
 
 def submit(username, password, address, params=None):
